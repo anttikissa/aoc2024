090f93f116d3579ee4f46ea2e1255ee0135e324a
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2024-12-03 22:49:11 +0200

    Day 3 cleanup

--- day3-2.ts
+++ day3-2.ts
@@ -8,17 +8,17 @@ let regex = /(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))/g
 let matches = input.match(regex)!
 
 let result = 0
-let enabled = true
+let enabled = 1
 
 for (let match of matches) {
-	if (match.startsWith('do()')) {
-		enabled = true
-	} else if (match.startsWith("don't()")) {
-		enabled = false
+	if (match === 'do()') {
+		enabled = 1
+	} else if (match === "don't()") {
+		enabled = 0
 	} else if (match.startsWith('mul')) {
 		let lhs = parseInt(match.slice(4))
 		let rhs = parseInt(match.slice(match.indexOf(',') + 1))
-		result += lhs * rhs * Number(enabled)
+		result += lhs * rhs * enabled
 	}
 }
 
