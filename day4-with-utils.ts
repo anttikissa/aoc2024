2a091162caa9e9977b13bb4cedf7a96dbefa56f8
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2024-12-04 15:37:17 +0200

    Use some utilities

--- day4-with-utils.ts
+++ day4-with-utils.ts
@@ -1,26 +1,62 @@
-import { coords, readFile, readLines, toGrid } from './utils.ts'
+import {
+	coords,
+	diagonalDirections,
+	directions,
+	get,
+	mul,
+	plus,
+	range,
+	readFile,
+	toGrid,
+} from './utils.ts'
 
 let log = console.log
 
 let grid: string[][] = toGrid(readFile('day4.txt'))
 
 let test = `
-.M.S......
-..A..MSMS.
-.M.S.MAA..
-..A.ASMSM.
-.M.S.M....
-..........
-S.S.S.S.S.
-.A.A.A.A..
-M.M.M.M.M.
-..........`
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`
 
-grid = toGrid(test)
+// grid = toGrid(test)
 
-let h = grid.length
-let w = grid[0].length
+// Part 1
+let result1 = 0
 
-for (let coord of coords(h, w)) {
-	log('coord', coord)
+for (let coord of coords(grid)) {
+	if (get(grid, coord) === 'X') {
+		for (let direction of directions) {
+			let letters = range(1, 3).map((i) =>
+				get(grid, plus(coord, mul(direction, i)))
+			)
+			if (letters.join('') === 'MAS') {
+				result1++
+			}
+		}
+	}
 }
+
+log('part 1', result1)
+
+// Part 2
+let result2 = 0
+let validCombinations = ['MMSS', 'SMMS', 'MSSM', 'SSMM']
+
+for (let coord of coords(grid)) {
+	if (get(grid, coord) === 'A') {
+		let rest = diagonalDirections.map((dir) => get(grid, plus(coord, dir)))
+		if (validCombinations.includes(rest.join(''))) {
+			result2++
+		}
+	}
+}
+
+log('part 2', result2)
--- utils.ts
+++ utils.ts
@@ -20,23 +20,62 @@ export function toNumbers(line: string) {
 	return line.split(/\s+/).map(Number)
 }
 
+// range(3) => [0, 1, 2] ("range of 3 elements")
+// range(1, 3) => [1, 2, 3] ("range of 1 to 4 inclusive")
 
-// 3 => [0, 1, 2]
-export function range(n: number) {
-	return Array(n)
-		.fill(0)
-		.map((_, i) => i)
+export function range(min: number, max: number): number[]
+export function range(n: number): number[]
+export function range(n: number, max?: number) {
+	if (typeof max === 'undefined') {
+		return Array(n)
+			.fill(0)
+			.map((_, i) => i)
+	} else {
+		let min = n
+		return Array(max - min + 1)
+			.fill(0)
+			.map((_, i) => i + min)
+	}
 }
 
-export function* coords(h: number, w: number) {
-	for (let i of range(h)) {
+export type Vec2 = [number, number]
+
+export const straightDirections: Vec2[] = [
+	[0, 1],
+	[-1, 0],
+	[0, -1],
+	[1, 0],
+]
+
+export const diagonalDirections: Vec2[] = [
+	[1, 1],
+	[1, -1],
+	[-1, -1],
+	[-1, 1],
+]
+
+export const directions: Vec2[] = [
+	...straightDirections,
+	...diagonalDirections,
+]
+
+export function coords(grid: unknown[][]): Generator<Vec2, void, unknown>
+export function coords(h: number, w: number): Generator<Vec2, void, unknown>
+export function *coords(hOrGrid: number | unknown[][], w?: number) {
+	if (typeof hOrGrid !== 'number') {
+		w = hOrGrid[0]?.length ?? 0
+		hOrGrid = hOrGrid.length
+	}
+	if (w == null) {
+		throw new Error('missing w')
+	}
+
+	for (let i of range(hOrGrid)) {
 		for (let j of range(w)) {
 			yield [i, j]
 		}
 	}
 }
-
-export type Vec2 = [number, number]
 
 export function plus([a, b]: Vec2, [c, d]: Vec2): Vec2 {
 	return [a + c, b + d]
@@ -46,6 +85,6 @@ export function mul([a, b]: Vec2, x: number): Vec2 {
 	return [a * x, b * x]
 }
 
-export function get(grid: string[][], [x, y]: Vec2) {
-	return grid[x]?.[y] || '.'
+export function get(grid: string[][], [y, x]: Vec2) {
+	return grid[y]?.[x] || '.'
 }
