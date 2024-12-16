d811b401daf74e3d3e7ef50b5628487d4cf7dd1a
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2024-12-16 18:26:23 +0200

    Optimize mightily (still no priority queue)

--- day16.ts
+++ day16.ts
@@ -7,12 +7,11 @@ import {
 	gridGet,
 	gridMap,
 	gridPrint,
-	gridSet,
 	log,
 	parseVec,
 	range,
-	RIGHT,
 	straightDirections,
+	timer,
 	toGrid,
 	ValueSet,
 	type Vec2,
@@ -61,10 +60,55 @@ let test2 = `
 #S#.............#
 #################`
 
-function solve(input: string, part = 1) {
+let test3 = `
+################
+#..............#
+#.############.#
+#E..##########.#
+###..#########.#
+##..##########.#
+#..###########.#
+#.############.#
+#S.............#
+################
+`
+
+let test4 = `
+#################
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#...###.#####.###
+#.#.#...#.....#.#
+#...#.#####.###.#
+#.#.#.........#.#
+#.E.#.#########.#
+#S#.............#
+#################
+`
+
+let unreachable = `
+####
+##E#
+#S##
+####
+`
+
+let unreachable2 = `
+####
+##.#
+####
+#SE#
+####
+`
+
+function solve(input: string, part: 1 | 2 = 1) {
 	let grid = toGrid(input)
 
-	let goal = coords(grid).find((c) => gridGet(grid, c) === 'E')!
+	let startPos = coords(grid).find((c) => gridGet(grid, c) === 'S')!
+	let goalPos = coords(grid).find((c) => gridGet(grid, c) === 'E')!
 
 	// Down left up right
 	type Dir = 0 | 1 | 2 | 3
@@ -87,17 +131,38 @@ function solve(input: string, part = 1) {
 		return [x, y, ((dir - 1 + 4) % 4) as Dir]
 	}
 
-	let unvisited = new ValueSet<Node>()
+	// TODO make priority queue later
+	let queue = new ValueSet<Node>([[startPos[0], startPos[1], 3]])
 
-	let seen = gridMap(grid, (c) => (c === '#' ? '#' : '.'))
-
-	for (let [x, y] of coords(grid)) {
-		let tile = gridGet(grid, [x, y])
-		if (tile === '#') continue
-		for (let dir of [0, 1, 2, 3]) {
-			unvisited.add([x, y, dir as Dir])
-		}
+	function addQueue(node: Node) {
+		queue.add(node)
 	}
+
+	function popNearestKnown(): [Node, number] {
+		// TODO optimize this
+		let nearestNode!: Node
+		let nearestNodeDistance = Infinity
+		for (let node of [...queue]) {
+			if (getDistance(node) < nearestNodeDistance) {
+				nearestNode = node
+				nearestNodeDistance = getDistance(node)
+			}
+		}
+
+		queue.delete(nearestNode)
+
+		return [nearestNode, nearestNodeDistance]
+	}
+
+	// let seen = gridMap(grid, (c) => (c === '#' ? '#' : '.'))
+
+	// for (let [x, y] of coords(grid)) {
+	// 	let tile = gridGet(grid, [x, y])
+	// 	if (tile === '#') continue
+	// 	for (let dir of [0, 1, 2, 3]) {
+	// 		queue.add([x, y, dir as Dir])
+	// 	}
+	// }
 
 	// [x, y] grid of [down, left, up, right] distances
 	let distances: number[][][] = gridMap(grid, (tile) => {
@@ -112,7 +177,7 @@ function solve(input: string, part = 1) {
 	function setDistance([x, y, dir]: Node, distance: number) {
 		let cell = gridGet(distances, [x, y])
 		if (gridGet(grid, [x, y]) === 'E') {
-			log('Set distance to goal', [x, y, dir], distance)
+			log('Set distance to goal', printNode([x, y, dir]), distance)
 		}
 		if (typeof cell === 'string') {
 			fail('setDistance', [x, y, dir], distance)
@@ -125,7 +190,7 @@ function solve(input: string, part = 1) {
 		return gridGet(grid, [x, y]) !== '#'
 	}
 
-	// Record of all visited nodes
+	// For debugging: record of all visited nodes
 	let trail: Node[] = []
 	let trailDistances: number[] = []
 
@@ -151,184 +216,117 @@ function solve(input: string, part = 1) {
 		}
 	}
 
-	function step(): 'continue' | number {
-		log(`step(): ${unvisited.size} remaining`)
-		if (unvisited.size === 0) {
-			if (part === 1) {
-				// Part 1
-				let distancesToEnd = range(4).map((dir) =>
-					getDistance([goal[0], goal[1], dir as Dir])
-				)
-				// log('distances to end', distancesToEnd)
-				let shortestDistance = Math.min(...distancesToEnd)
-				// log('empty unvisited, return', shortestDistance)
-				return shortestDistance
-			}
+	let steps = 0
+	let goalsFound = 0
 
-			if (part === 2) {
-				// Part 2
-				let finalNodes = range(4).map(
-					(dir) => [goal[0], goal[1], dir as Dir] as Node
-				)
-				finalNodes.sort((n1, n2) => getDistance(n1) - getDistance(n2))
-				log('finalNodes', finalNodes.map(printNode))
-				// let node = printNode(finalNodes[0])
+	function finish() {
+		if (part === 1) {
+			// Part 1
+			let distancesToEnd = range(4).map((dir) =>
+				getDistance([goalPos[0], goalPos[1], dir as Dir])
+			)
+			return Math.min(...distancesToEnd)
+		}
 
-				let seen = new Set<string>()
-				let unvisited: string[] = [printNode(finalNodes[0])]
-				while (unvisited.length) {
-					let node = unvisited.pop()!
-					seen.add(node)
+		if (part === 2) {
+			// Part 2
+			let finalNodes = range(4).map(
+				(dir) => [goalPos[0], goalPos[1], dir as Dir] as Node
+			)
+			finalNodes.sort((n1, n2) => getDistance(n1) - getDistance(n2))
+			// log('finalNodes', finalNodes.map(printNode))
+			// let node = printNode(finalNodes[0])
 
-					let predecessors = optimalPredecessors.get(node)!
-					if (!predecessors) {
-						log('no predecessors for', node)
-						continue
-					}
-					let idx = 0
-					for (let predecessor of predecessors) {
-						log(`predecessor ${idx++} of ${node}:`, predecessor)
-						unvisited.push(predecessor)
-					}
+			let seen = new Set<string>()
+			let unvisited: string[] = [printNode(finalNodes[0])]
+			while (unvisited.length) {
+				let node = unvisited.pop()!
+				seen.add(node)
+
+				let predecessors = optimalPredecessors.get(node)!
+				if (!predecessors) {
+					// log('no predecessors for', node)
+					continue
 				}
-
-				log('seen', seen.size)
-				log('set of seen', seen)
-
-				let withoutDir = new ValueSet<Vec2>(
-					[...seen].map((node) => parseVec(node.slice(0, -1)))
-				)
-
-				log('seen', withoutDir.size)
-				log('set of seen', withoutDir)
-
-				log('grid\n' + gridPrint(grid, 'O', withoutDir))
-
-				return withoutDir.size
-				// log('shortest', printNode(node))
-
-				// return 0
+				let idx = 0
+				for (let predecessor of predecessors) {
+					// log(`predecessor ${idx++} of ${node}:`, predecessor)
+					unvisited.push(predecessor)
+				}
 			}
+
+			let seenPositions = new ValueSet<Vec2>(
+				[...seen].map((node) => parseVec(node.slice(0, -1)))
+			)
+
+			// log('Optimal paths:\n' + gridPrint(grid, 'O', seenPositions))
+
+			return seenPositions.size
 		}
 
-		// TODO optimize this
+		return 0
+	}
 
-		let nearestNode = undefined
-		let nearestNodeDistance = Infinity
-		for (let node of [...unvisited]) {
-			if (getDistance(node) < nearestNodeDistance) {
-				nearestNode = node
-				nearestNodeDistance = getDistance(node)
-			}
+	function step(): 'continue' | number {
+		steps++
+
+		// log(`step(): ${queue.size} remaining`)
+		if (queue.size === 0) {
+			log('Exhausted search space at step', steps)
+			return finish()
 		}
+
+		let [nearestNode, nearestNodeDistance] = popNearestKnown()
+
 		if (!nearestNode) {
 			fail('oops')
 		}
 
-		// let nodesSorted = [...unvisited].sort((n1: Node, n2: Node) => {
-		// 	let d1 = getDistance(n1)
-		// 	let d2 = getDistance(n2)
-		//
-		// 	// @ts-ignore
-		// 	return d1 - d2
-		// })
-		//
-		// let nearestNode = nodesSorted[0]
-		// let nearestNodeDistance = getDistance(nearestNode)
-
 		if (nearestNodeDistance === Infinity) {
-			log('TODO considering a node that is unreachable, can this happen?')
-			unvisited.delete(nearestNode)
-			return 'continue'
+			fail('should not happen')
 		}
 
 		trail.push(nearestNode)
 		trailDistances.push(nearestNodeDistance)
 
 		if (gridGet(grid, [nearestNode[0], nearestNode[1]]) === 'E') {
-			log('Found goal', nearestNodeDistance)
-			// Not yet
-			// return nearestNodeDistance
+			goalsFound++
+
+			log('Found goal', nearestNodeDistance, 'at step', steps)
+
+			if (goalsFound === 4) {
+				log('All goals found at step', steps)
+				// return finish()
+			}
 		}
 
-		// Do magic
 		let straight: Node = moveStraight(nearestNode)
 		let left: Node = turnLeft(nearestNode)
 		let right: Node = turnRight(nearestNode)
-		// log('Considering node', printNode(nearestNode), {
-		// 	straight: printNode(straight),
-		// 	left: printNode(left),
-		// 	right: printNode(right),
-		// 	dist: nearestNodeDistance,
-		// })
+
+		function add(node: Node, distance: number) {
+			let previousDistance = getDistance(node)
+			if (distance <= previousDistance) {
+				setDistance(node, distance)
+				addQueue(node, distance)
+				addOptimalPredecessor(
+					node,
+					nearestNode,
+					distance < previousDistance
+				)
+			}
+		}
+
 		if (isValidPos(straight)) {
-			let currDist = getDistance(straight)
-			let newDist = nearestNodeDistance + 1
-			if (newDist == currDist) {
-				addOptimalPredecessor(straight, nearestNode)
-			}
-			if (newDist < currDist) {
-				// 	log(
-				// 		'New shortest path to',
-				// 		printNode(straight),
-				// 		'found:',
-				// 		newDist,
-				// 		'(go straight)'
-				// 	)
-				setDistance(straight, newDist)
-				addOptimalPredecessor(straight, nearestNode, true)
-			}
+			add(straight, nearestNodeDistance + 1)
 		}
-		let currDistLeft = getDistance(left)
-		let newDistLeft = nearestNodeDistance + 1000
-		if (newDistLeft === currDistLeft) {
-			addOptimalPredecessor(left, nearestNode)
-		}
-		if (newDistLeft < currDistLeft) {
-			// log(
-			// 	'New shortest path to',
-			// 	printNode(left),
-			// 	'found:',
-			// 	newDistLeft,
-			// 	'(turn left)'
-			// )
+		add(left, nearestNodeDistance + 1000)
+		add(right, nearestNodeDistance + 1000)
 
-			setDistance(left, newDistLeft)
-			addOptimalPredecessor(left, nearestNode, true)
-		}
-
-		let currDistRight = getDistance(right)
-		let newDistRight = nearestNodeDistance + 1000
-		// if (printNode(right) === '1,11R') {
-		// 	log('1,11R', { currDistRight, newDistRight })
-		// }
-
-		if (newDistRight === currDistRight) {
-			addOptimalPredecessor(right, nearestNode)
-		}
-		if (newDistRight < currDistRight) {
-			// log(
-			// 	'New shortest path to',
-			// 	printNode(right),
-			// 	'found:',
-			// 	newDistRight,
-			// 	'(turn right)'
-			// )
-
-			setDistance(right, newDistRight)
-			addOptimalPredecessor(right, nearestNode, true)
-		}
-
-		gridSet(seen, [nearestNode[0], nearestNode[1]], 'X')
-
-		unvisited.delete(nearestNode)
-		// return 'stop'
+		// queue.delete(nearestNode)
+		steps++
 		return 'continue'
 	}
-
-	// log('grid', gridPrint(grid))
-	// log('dist', gridPrint(distances))
-	// log('unvisited', unvisited)
 
 	while (true) {
 		let result = step()
@@ -337,8 +335,6 @@ function solve(input: string, part = 1) {
 		}
 
 		// printTrail()
-		// log('!!! RESULT', result)
-
 		return result
 	}
 
@@ -361,12 +357,22 @@ function solve(input: string, part = 1) {
 	}
 }
 
-assert(solve(test), 7036)
-assert(solve(test2), 11048)
-assert(solve(input), 111480)
+// assert(solve(unreachable), Infinity)
+// assert(solve(unreachable2), 1)
 
-assert(solve(test, 2), 45)
-assert(solve(test2, 2), 64)
-assert(solve(input, 2), 529)
+// assert(solve(test4), 7036)
+
+{
+	using part1 = timer('part1')
+	assert(solve(test), 7036)
+	assert(solve(test2), 11048)
+	assert(solve(input), 111480)
+}
+{
+	using part2 = timer('part2')
+	assert(solve(test, 2), 45)
+	assert(solve(test2, 2), 64)
+	assert(solve(input, 2), 529)
+}
 
 log('ok')
