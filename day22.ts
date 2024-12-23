ad37b70002d78a92b59d606b9a14f58f461dd384
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2024-12-23 07:18:20 +0200

    Day 22 part 2

--- day22.ts
+++ day22.ts
@@ -1,5 +1,5 @@
 import input from './day22.txt'
-import { assert, log, range, timer } from './utils'
+import { assert, cache, log, max, range, timer } from './utils'
 
 let test = `
 1
@@ -7,36 +7,62 @@ let test = `
 100
 2024`
 
-function mixAndPrune(a: number, b: number) {
-	return Number(BigInt(a) ^ BigInt(b)) % 16777216
-}
+let test2 = `
+1
+2
+3
+2024`
 
-function next(secret: number) {
+let next = (secret: number) => {
+	let sec = secret
+
 	let a = secret * 64
-	secret = mixAndPrune(secret, a)
-	let b = Math.floor(secret / 32)
-	secret = mixAndPrune(secret, b)
+	secret = ((secret ^ a) >>> 0) % 16777216
+	let b = (secret / 32) | 0
+	secret = ((secret ^ b) >>> 0) % 16777216
 	let c = secret * 2048
-	secret = mixAndPrune(secret, c)
+	secret = ((secret ^ c) >>> 0) % 16777216
 
 	return secret
 }
 
-function solve(input: string, n: number) {
+function solve(input: string, n: number, part: 1 | 2 = 1) {
 	let seeds = input.split('\n').filter(Boolean).map(Number)
 
-	let result = 0
-	for (let seed of seeds) {
-		let num = seed
-		for (let _ of range(n)) {
-			num = next(num)
+	if (part === 1) {
+		let result = 0
+		for (let seed of seeds) {
+			let num = seed
+			for (let _ of range(n)) {
+				num = next(num)
+			}
+			result += num
 		}
-		result += num
-		// log(`${seed}: ${num}`)
+		return result
 	}
 
-	return result
-	// return seeds.length
+	if (part === 2) {
+		let totalsByMagic = Array(32 ** 4).fill(0)
+		for (let seed of seeds) {
+			let prevPrice = seed % 10
+			let secret = seed
+			let magic = 0
+			let magicsSeen = new Set()
+			for (let _ of range(n)) {
+				secret = next(secret)
+				let price = secret % 10
+				let diff = prevPrice - price
+				magic = ((magic << 5) + (diff + 10)) & (32 ** 4 - 1)
+				if (!magicsSeen.has(magic)) {
+					totalsByMagic[magic] += price
+				}
+				magicsSeen.add(magic)
+				prevPrice = price
+			}
+		}
+
+		return max(totalsByMagic)
+	}
 }
 
 assert(next(123), 15887950)
@@ -45,8 +71,15 @@ assert(next(15887950), 16495136)
 // 5864043 too low
 assert(solve(test, 2000), 37327623)
 {
-	using perf = timer('input')
+	using perf = timer('part 1')
 	assert(solve(input, 2000), 20215960478)
+}
+
+assert(solve(test2, 2000, 2), 23)
+
+{
+	using perf = timer('part 2')
+	assert(solve(input, 2000, 2), 2221)
 }
 
 log('ok')
